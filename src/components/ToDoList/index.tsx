d42e41198b49b60a169979059d9ae7deb73e22eb
yushu <yushu5352@163.com>
2021-01-18 15:56:25 +0800

    project init

---
+++ src/components/ToDoList/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC, ReactElement } from 'react';
+
+const ToDoList: FC = (): ReactElement => {
+    return (
+        <div className='todolist'>
+            ToDoList
+        </div>
+    );
+}
+
+export default ToDoList;
